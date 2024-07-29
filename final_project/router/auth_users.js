e52eb223226ab7039e70c354c2d2254b20516481
Sadie <an5dy90@gmail.com>
2024-07-29 05:03:21 -0400

    put tested and deleted tested.

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -47,6 +47,17 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   return res.status(404).json({message: "Book with isbn not found"});
 });
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    for (let [num, book] of Object.entries(books)) {
+        if (num === isbn) {
+            book.reviews = {};
+            return res.status(200).json({message: "Review deleted"});
+        }
+    }
+    return res.status(404).json({message: "Book not found"});
+})
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
